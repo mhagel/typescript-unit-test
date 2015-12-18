6162d353f94a720bb207da37cd9647c29c5b94e6
Mark Hagelberg <mark.hagelberg@briebug.com>
2015-12-17 17:03:23 -0700

    angular-mocks

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      {pattern: 'src/**/*.spec.js', included: false}
+      {pattern: '**/*.spec.js', included: false}
     ],
 
 
